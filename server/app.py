08547f1c9dd1a3ced3097ecf4b7829a2587de759
Erin Wise <erinwise88@gmail.com>
2024-02-09 13:29:35 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,73 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+#GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        
+        response = make_response(messages, 200)
+
+        return response
+    
+    elif request.method == "POST":
+        data = request.get_json()
+
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(message_dict, 201)
+
+        return response
+
+
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This record does not exist"
+        }
+
+        response = make_response(response_body, 400)
+
+        return response
+    else:
+        if request.method == "GET":
+            message_dict = message.to_dict()
+            response = make_response(message_dict, 200)
+            return response
+        elif request.method == "PATCH":
+            data = request.get_json()
+            for attr in data:
+                setattr(message, attr, data[attr])
+            
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+            response = make_response(message_dict, 200)
+            return response
+        elif request.method == "DELETE":
+            db.session.delete(message)
+            db.session.commit()
+
+            response = make_response({'deleted': True}, 200)
+            return response
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/568365d8066e_added_message_attributes.py
@@ -0,0 +1,24 @@
+"""added Message attributes
+
+Revision ID: 568365d8066e
+Revises: 
+Create Date: 2024-02-09 12:43:40.405998
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '568365d8066e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
